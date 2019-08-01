32fd085041ae96e7ce3b9b33abf0accdfa006220
El_Mitya32 <el_mitya@mail.ru>
2019-08-01 10:07:58 +0300

    fisrtss commit

--- app/src/main/java/com/bignerdranch/android/rusticfuns/MainActivity.java
+++ app/src/main/java/com/bignerdranch/android/rusticfuns/MainActivity.java
@@ -2,6 +2,7 @@ package com.bignerdranch.android.rusticfuns;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button1;
+        Button button2;
+        Button button3;
+
+        button1 = (Button) findViewById(R.id.button1);
+        button2 = (Button) findViewById(R.id.button2);
+        button3 = (Button) findViewById(R.id.button3);
+
+        
     }
 }
